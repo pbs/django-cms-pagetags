56daeab19db6a568aaa9bd8a13325c8a46f3c4d0
Furau Calin <calin.furau@gmail.com>
2011-11-18 17:42:41 +0200

    Implemented templatetag used to iterate trough pages which have a specified tag or list of tags.

---
+++ pagetags/templatetags/page_tags.py
@@ -0,0 +1,36 @@
+import re
+
+from django import template
+from cms.models import Page
+
+from tagging.models import TaggedItem
+from pagetags.models import PageTagging
+
+
+class TaggedPagesNode(template.Node):
+    def __init__(self, format_string, var_name):
+        self.format_string = format_string
+        self.var_name = var_name
+    def render(self, context):
+        tagged_pages = TaggedItem.objects.get_by_model(PageTagging, self.format_string)
+        result_pages = [Page.objects.get(pk=tagged_page.page_id) for tagged_page in tagged_pages]
+        context[self.var_name] = result_pages
+        return ''
+
+
+def get_tagged_pages(parser, token):
+    try:
+        tag_name, arg = token.contents.split(None, 1)
+    except ValueError:
+        raise template.TemplateSyntaxError("%r tag requires a single argument" % token.contents.split()[0])
+    m = re.search(r'(.*?) as (\w+)', arg)
+    if not m:
+        raise template.TemplateSyntaxError("%r tag had invalid arguments" % tag_name)
+    format_string, var_name = m.groups()
+    if not (format_string[0] == format_string[-1] and format_string[0] in ('"', "'")):
+        raise template.TemplateSyntaxError("%r tag's argument should be in quotes" % tag_name)
+    return TaggedPagesNode(format_string[1:-1], var_name)
+
+
+register = template.Library()
+register.tag('pages_with_tags', get_tagged_pages)
