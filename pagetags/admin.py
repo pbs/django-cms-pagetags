f7edd8ece1da26854b3d4111db8409dfdf7aece3
Furau Calin <calin.furau@gmail.com>
2011-11-21 11:05:03 +0200

    Better integration with other modeladmins.

--- pagetags/admin.py
+++ pagetags/admin.py
@@ -1,20 +1,27 @@
 from django.contrib import admin
 from django.contrib.admin.sites import NotRegistered
 
-from cms.admin.pageadmin import PageAdmin
 from cms.models.pagemodel import Page
 
 from models import PageTagging
+
+
+def _get_registered_modeladmin(model):
+    """ This is a huge hack to get the registered modeladmin for the model.
+        We need this functionality in case someone else already registered
+        a different modeladmin for this model. """
+    return type(admin.site._registry[model])
 
 
 class PageTaggingAdmin(admin.TabularInline):
     model = PageTagging
 
 
-PageAdmin.inlines.append(PageTaggingAdmin)
+RegisteredPageAdmin = _get_registered_modeladmin(Page)
+RegisteredPageAdmin.inlines.append(PageTaggingAdmin)
 
 try:
     admin.site.unregister(Page)
 except NotRegistered:
     pass
-admin.site.register(Page, PageAdmin)
+admin.site.register(Page, RegisteredPageAdmin)
