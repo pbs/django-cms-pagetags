168351cf929e3856063899d629f79f4949ac4402
Furau Calin <calin.furau@gmail.com>
2011-11-22 17:41:08 +0200

    pages_similar_with templatetag takes the site specific unique page slug as an argument.

--- pagetags/templatetags/page_tags.py
+++ pagetags/templatetags/page_tags.py
@@ -2,6 +2,7 @@ import re
 
 from django import template
 from django.conf import settings
+from django.db.models import Q
 from cms.models import Page
 
 from tagging.models import TaggedItem
@@ -27,7 +28,8 @@ class SimilarPagesNode(template.Node):
         self.format_string = format_string
         self.var_name = var_name
     def render(self, context):
-        page = Page.objects.get(title_set__title=self.format_string)
+        page = Page.objects.get(
+            Q(site=settings.SITE_ID), Q(title_set__slug=self.format_string))
         result_pages = TaggedItem.objects.get_related(
             page.pagetagging, PageTagging.objects.filter(page__site=settings.SITE_ID))
         context[self.var_name] = [page_tagging.page for page_tagging in result_pages]
@@ -35,20 +37,16 @@ class SimilarPagesNode(template.Node):
 
 
 def get_tagged_pages(parser, token):
-    try:
-        tag_name, arg = token.contents.split(None, 1)
-    except ValueError:
-        raise template.TemplateSyntaxError("%r tag requires a single argument" % token.contents.split()[0])
-    m = re.search(r'(.*?) as (\w+)', arg)
-    if not m:
-        raise template.TemplateSyntaxError("%r tag had invalid arguments" % tag_name)
-    format_string, var_name = m.groups()
-    if not (format_string[0] == format_string[-1] and format_string[0] in ('"', "'")):
-        raise template.TemplateSyntaxError("%r tag's argument should be in quotes" % tag_name)
-    return TaggedPagesNode(format_string[1:-1], var_name)
+    format_string, var_name = _validate_template(parser, token)
+    return TaggedPagesNode(format_string, var_name)
 
 
 def get_similar_pages(parser, token):
+    format_string, var_name = _validate_template(parser, token)
+    return SimilarPagesNode(format_string, var_name)
+
+
+def _validate_template(parser, token):
     try:
         tag_name, arg = token.contents.split(None, 1)
     except ValueError:
@@ -59,7 +57,7 @@ def get_similar_pages(parser, token):
     format_string, var_name = m.groups()
     if not (format_string[0] == format_string[-1] and format_string[0] in ('"', "'")):
         raise template.TemplateSyntaxError("%r tag's argument should be in quotes" % tag_name)
-    return SimilarPagesNode(format_string[1:-1], var_name)
+    return format_string[1:-1], var_name
 
 
 register = template.Library()
