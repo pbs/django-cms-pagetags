0514eab1c4ee00e62aef75e4e01ec12591ce123d
Sever Băneşiu <banesiu.sever@gmail.com>
2011-11-28 15:43:05 +0200

    Added autocomplete view and added js from google.

--- pagetags/views.py
+++ pagetags/views.py
@@ -1,1 +1,15 @@
 # Create your views here.
+
+from django.http import HttpResponse
+from tagging.models import Tag
+import json
+
+def list_tags(request):
+        q = request.GET.get('q')
+        tags = []
+        if q:
+            tags = (Tag.objects
+                    .filter(name__istartswith=q)
+                    .values_list('name', flat=True))
+        response = [dict(id=tag, label=tag, value=tag) for tag in tags]
+        return HttpResponse(json.dumps(response), mimetype='application/json')
