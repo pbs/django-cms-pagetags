924f92c4dcf4f721c74d345a51ad12d7606a23ba
Calin Furau <calin.furau@gmail.com>
2011-11-30 00:55:05 +0200

    Added sortind functionality for pages_with_tags templatetags.

--- pagetags/templatetags/page_tags.py
+++ pagetags/templatetags/page_tags.py
@@ -14,9 +14,10 @@ register = template.Library()
 
 
 class TaggedPagesNode(template.Node):
-    def __init__(self, format_string, var_name):
+    def __init__(self, format_string, ordering, var_name):
         self.format_string = format_string
         self.var_name = var_name
+        self.ordering = ordering
 
     def render(self, context):
         parsed_tags = parse_tag_input(self.format_string)
@@ -24,6 +25,16 @@ class TaggedPagesNode(template.Node):
         pagetaggings_from_site = PageTagging.objects.filter(
             page__site=settings.SITE_ID
         ).select_related()
+
+        if self.ordering == 'alphabetical':
+            pagetaggings_from_site = pagetaggings_from_site.order_by(
+                'page__title_set__slug'
+            )
+
+        elif self.ordering == 'chronological':
+            pagetaggings_from_site = pagetaggings_from_site.order_by(
+                'page__publication_date'
+            ).reverse()
 
         pagetaggings = TaggedItem.objects.get_by_model(
             pagetaggings_from_site, parsed_tags
@@ -40,7 +51,6 @@ class SimilarPagesNode(template.Node):
         self.var_name = var_name
 
     def render(self, context):
-
         try:
             page = Page.objects.get(
                 Q(site=settings.SITE_ID),
@@ -55,30 +65,22 @@ class SimilarPagesNode(template.Node):
                 page__site=settings.SITE_ID
             )
         )
-
         context[self.var_name] = [pt.page for pt in pagetaggings]
         return ''
 
 
 @register.tag
 def pages_with_tags(parser, token):
-    format_string, var_name = _validate_template(parser, token)
-    return TaggedPagesNode(format_string, var_name)
+    format_string, ordering, var_name = _parse_templatetag(parser, token)
+    return TaggedPagesNode(format_string, ordering, var_name)
 
 @register.tag
 def pages_similar_with(parser, token):
-    format_string, var_name = _validate_template(parser, token)
+    format_string, var_name = _parse_templatetag(parser, token)
     return SimilarPagesNode(format_string, var_name)
 
-def _validate_template(parser, token):
-    try:
-        tag_name, arg = token.contents.split(None, 1)
-    except ValueError:
-        raise template.TemplateSyntaxError("%r tag requires a single argument" % token.contents.split()[0])
-    m = re.search(r'(.*?) as (\w+)', arg)
-    if not m:
-        raise template.TemplateSyntaxError("%r tag had invalid arguments" % tag_name)
-    format_string, var_name = m.groups()
-    if not (format_string[0] == format_string[-1] and format_string[0] in ('"', "'")):
-        raise template.TemplateSyntaxError("%r tag's argument should be in quotes" % tag_name)
-    return format_string[1:-1], var_name
+def _parse_templatetag(parser, token):
+    tag_name, arg = token.contents.split(None, 1)
+    m = re.search(r'(.*?) order (\w+) as (\w+)', arg)
+    format_string, ordering, var_name = m.groups()
+    return format_string[1:-1], ordering, var_name
--- pagetags/views.py
+++ pagetags/views.py
@@ -5,11 +5,11 @@ from tagging.models import Tag
 import json
 
 def list_tags(request):
-        q = request.GET.get('q')
-        tags = []
-        if q:
-            tags = (Tag.objects
-                    .filter(name__istartswith=q)
-                    .values_list('name', flat=True))
-        response = [dict(id=tag, label=tag, value=tag) for tag in tags]
-        return HttpResponse(json.dumps(response), mimetype='application/json')
+    q = request.GET.get('q')
+    tags = []
+    if q:
+        tags = (Tag.objects
+                .filter(name__istartswith=q)
+                .values_list('name', flat=True))
+    response = [dict(id=tag, label=tag, value=tag) for tag in tags]
+    return HttpResponse(json.dumps(response), mimetype='application/json')
