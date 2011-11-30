075bbc5f64e634fa763c3c9e73bdc9e5b8bbd1e1
Furau Calin <calin.furau@gmail.com>
2011-11-30 10:53:23 +0200

    Modified regex for better templatetag parsing.

--- pagetags/templatetags/page_tags.py
+++ pagetags/templatetags/page_tags.py
@@ -26,29 +26,31 @@ class TaggedPagesNode(template.Node):
             page__site=settings.SITE_ID
         ).select_related()
 
-        if self.ordering == 'alphabetical':
+        if self.ordering and 'alphabetical' in self.ordering:
             pagetaggings_from_site = pagetaggings_from_site.order_by(
                 'page__title_set__slug'
             )
 
-        elif self.ordering == 'chronological':
+        elif self.ordering and 'chronological' in self.ordering:
             pagetaggings_from_site = pagetaggings_from_site.order_by(
                 'page__publication_date'
             ).reverse()
 
-        pagetaggings = TaggedItem.objects.get_by_model(
+        page_taggings = TaggedItem.objects.get_by_model(
             pagetaggings_from_site, parsed_tags
         )
 
-        context[self.var_name] = [pt.page for pt in pagetaggings]
+        context[self.var_name] = [
+            page_tagging.page for page_tagging in page_taggings
+        ]
         return ''
 
 
 class SimilarPagesNode(template.Node):
-
-    def __init__(self, format_string, var_name):
+    def __init__(self, format_string, ordering, var_name):
         self.format_string = format_string
         self.var_name = var_name
+        self.ordering = ordering
 
     def render(self, context):
         try:
@@ -59,13 +61,15 @@ class SimilarPagesNode(template.Node):
         except Page.DoesNotExist:
             return ''
 
-        pagetaggings = TaggedItem.objects.get_related(
+        page_taggings = TaggedItem.objects.get_related(
             page.pagetagging,
             PageTagging.objects.select_related().filter(
                 page__site=settings.SITE_ID
             )
         )
-        context[self.var_name] = [pt.page for pt in pagetaggings]
+        context[self.var_name] = [
+            page_tagging.page for page_tagging in page_taggings
+        ]
         return ''
 
 
@@ -76,11 +80,28 @@ def pages_with_tags(parser, token):
 
 @register.tag
 def pages_similar_with(parser, token):
-    format_string, var_name = _parse_templatetag(parser, token)
-    return SimilarPagesNode(format_string, var_name)
+    format_string, ordering, var_name = _parse_templatetag(parser, token)
+    return SimilarPagesNode(format_string, ordering, var_name)
 
 def _parse_templatetag(parser, token):
-    tag_name, arg = token.contents.split(None, 1)
-    m = re.search(r'(.*?) order (\w+) as (\w+)', arg)
+    try:
+        tag_name, arg = token.contents.split(None, 1)
+    except ValueError:
+        raise template.TemplateSyntaxError(
+            "%r tag requires additional arguments" % token.contents.split()[0]
+        )
+
+    m = re.search(r'(.*?) (order \w+ )*as (\w+)', arg)
+    if not m:
+        raise template.TemplateSyntaxError(
+            "%r tag had invalid arguments" % tag_name
+        )
+
     format_string, ordering, var_name = m.groups()
+    if not (format_string[0] == format_string[-1]
+        and format_string[0] in ('"', "'")):
+        raise template.TemplateSyntaxError(
+            "%r tag's argument should be in quotes" % tag_name
+        )
+
     return format_string[1:-1], ordering, var_name
