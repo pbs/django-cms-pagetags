a73b06dde7ff71343f321686b34a47cd8522e35b
gw0 [http://gw.tnode.com/] <gw.2012@tnode.com>
2012-08-05 00:10:27 +0200

    Code and documentation cleanup according to `djperfect_app` template.

--- pagetags/admin.py
+++ pagetags/admin.py
@@ -1,39 +1,29 @@
-import random
-import string
-from django.forms.widgets import TextInput
-from django.utils.safestring import mark_safe
+# -*- coding: utf-8 -*-
+"""
+Admin interface for Pagetags Django CMS plugin.
+"""
 from django.contrib import admin
 from django.contrib.admin.sites import NotRegistered
-from django.core.urlresolvers import reverse
-
-
-from tagging.fields import TagField
 from cms.models.pagemodel import Page
 
-from models import PageTagging
+from tagging.fields import TagField
+from pagetags import models
+from pagetags.fields import AutocompleteWidget
 
 
 def _get_registered_modeladmin(model):
-    """ This is a huge hack to get the registered modeladmin for the model.
-        We need this functionality in case someone else already registered
-        a different modeladmin for this model. """
+    """Huge hack to get the registered modeladmin for the model.
+    
+    We need this functionality in case someone else already registered a
+    different modeladmin for this model.
+    """
     return type(admin.site._registry[model])
 
 
-class AutocompleteWidget(TextInput):
-    def render(self, *args, **kwargs):
-        id = (kwargs
-                .get('attrs', {})
-                .get('id', random.sample(string.letters, 15)))
-        html = super(AutocompleteWidget, self).render(*args, **kwargs)
-        url = reverse('pagetags.views.list_tags')
-        js = '<script type="text/javascript">autocomplete("%s", "%s")</script>'
-        js %= (id, url)
-        return mark_safe(html + js)
-
-
 class PageTaggingAdmin(admin.StackedInline):
-    model = PageTagging
+    """Inline admin interface for `PageTagging` model."""
+
+    model = models.PageTagging
     fields = ('page_tags',)
     formfield_overrides = {
         TagField: {
@@ -41,8 +31,7 @@ class PageTaggingAdmin(admin.StackedInline):
         }
     }
 
-
-
+# Append inline admin to PageAdmin
 RegisteredPageAdmin = _get_registered_modeladmin(Page)
 RegisteredPageAdmin.inlines.append(PageTaggingAdmin)
 RegisteredPageAdmin.change_form_template = 'pagetags/change_form.html'
@@ -52,3 +41,4 @@ try:
 except NotRegistered:
     pass
 admin.site.register(Page, RegisteredPageAdmin)
+
--- pagetags/models.py
+++ pagetags/models.py
@@ -1,8 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Models for Pagetags Django CMS plugin.
+"""
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 from cms.models.pagemodel import Page
 
 from tagging.fields import TagField
 
+
 class PageTagging(models.Model):
-    page = models.OneToOneField(Page)
-    page_tags = TagField()
+    """Model for `django-tagging` integration with Django CMS pages."""
+
+    page = models.OneToOneField(Page, verbose_name=_('Page'))
+    page_tags = TagField(verbose_name=_('Tags'),
+        help_text=_("Please provide a comma-separated list of tags."))
+
+    class Meta:
+        verbose_name = _('Tag')
+        verbose_name_plural = _('Tags')
+
+    def __unicode__(self):
+        return u'Tags on {0}'.format(self.page)
+
